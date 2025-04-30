7b7d6c38fdf11e19a9851d1eea0b033cb409cf3a
Ahmed Tarek <atarekmokhtar90@gmail.com>
2025-04-30 19:26:14 +0200

    Create install-vbox-ubuntu.sh

---
+++ setup/vm-install-script/install-vbox-ubuntu.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+echo ".........----------------#################._.-.-Caution-.-._.#################----------------........."
+echo "If you are running this script on a virtualbox vm and you want to set a maunal ip address then follow the steps:"
+echo "    - First set the network adapter to bridged"
+echo "    - Use the template in network-config-template"
+echo "    - sudo netplan apply"
+echo "#######################################################################################################"
+
+
+echo ".........----------------#################._.-.-INSTALL-.-._.#################----------------........."
+PS1='\[\e[01;36m\]\u\[\e[01;37m\]@\[\e[01;33m\]\H\[\e[01;37m\]:\[\e[01;32m\]\w\[\e[01;37m\]\$\[\033[0;37m\] '
+echo "PS1='\[\e[01;36m\]\u\[\e[01;37m\]@\[\e[01;33m\]\H\[\e[01;37m\]:\[\e[01;32m\]\w\[\e[01;37m\]\$\[\033[0;37m\] '" >> ~/.bashrc
+sed -i '1s/^/force_color_prompt=yes\n/' ~/.bashrc
+source ~/.bashrc
+
+apt-get autoremove -y  #removes the packages that are no longer needed
+apt-get update
+systemctl daemon-reload
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /" | tee /etc/apt/sources.list.d/kubernetes.list
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+apt-get update
+apt-get install -y docker.io vim build-essential jq jc python3-pip kubelet kubectl kubernetes-cni kubeadm
+
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "storage-driver": "overlay2"
+}
+EOF
+mkdir -p /etc/systemd/system/docker.service.d
+
+systemctl daemon-reload
+systemctl restart docker
+systemctl enable docker
+systemctl enable kubelet
+systemctl start kubelet
+
+echo ".........----------------#################._.-.-KUBERNETES-.-._.#################----------------........."
+rm /root/.kube/config
+kubeadm reset -f
+
+# uncomment below line if your host doesnt have minimum requirement of 2 CPU
+# kubeadm init --kubernetes-version=${KUBE_VERSION} --ignore-preflight-errors=NumCPU --skip-token-print
+kubeadm init --skip-token-print
+
+mkdir -p ~/.kube
+sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
+
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+
+sleep 60
+
+echo "untaint controlplane node"
+kubectl taint node $(kubectl get nodes -o=jsonpath='{.items[].metadata.name}') node.kubernetes.io/not-ready:NoSchedule-
+kubectl taint node $(kubectl get nodes -o=jsonpath='{.items[].metadata.name}') node-role.kubernetes.io/master:NoSchedule-
+kubectl get node -o wide
+
+
+
+echo ".........----------------#################._.-.-Java and MAVEN-.-._.#################----------------........."
+sudo apt install openjdk-17-jdk -y
+java -version
+sudo apt install -y maven
+mvn -v
+
+
+
+echo ".........----------------#################._.-.-JENKINS-.-._.#################----------------........."
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /etc/apt/keyrings/jenkins-keyring.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev$
+wget http://archive.ubuntu.com/ubuntu/pool/main/i/init-system-helpers/init-system-helpers_1.56+nmu1~ubuntu18.04.1_all.deb
+sudo dpkg -i init-system-helpers_1.56+nmu1~ubuntu18.04.1_all.deb
+sudo apt update
+sudo apt install -y jenkins
+systemctl daemon-reload
+systemctl enable jenkins
+sudo systemctl start jenkins
+#sudo systemctl status jenkins
+sudo usermod -a -G docker jenkins
+echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
